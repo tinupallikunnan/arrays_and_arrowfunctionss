684e3e19d58bc6ea285a79672e18f89bb7d2f8ca
Tinu <149411428+tinupallikunnan@users.noreply.github.com>
2023-12-04 16:10:08 +0000

    first commit

---
+++ reverseArray.js
@@ -0,0 +1,7 @@
+
+
+let reverseArray = array => array.reverse();
+
+
+console.log(reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(reverseArray(["I", "like", "JavaScript"]));
---
+++ reverseString.js
@@ -0,0 +1,12 @@
+
+
+let reverseString = str => str.split("").reverse().join("");
+
+//split() J,o,h,n
+// reverse() : reverses arrays but not strings n,h,o,n
+//join()
+// join all elements of the array into a string
+
+
+console.log(reverseString("John"));
+console.log(reverseString("James"));
